616d53b9d89649c0586fb41643fb436f3537cfa0
Anya <use.anya@gmail.com>
2022-04-11 22:44:51 +0300

    Добавьте файлы проекта.

---
+++ 5.1/main.cpp
@@ -0,0 +1,207 @@
+#include <iostream>
+
+using namespace std;
+
+//ƒан линейный однонаправленный список L
+//1) –азработать функцию, котора€ создает из значений узлов списка L два новых списка: L1 Ц из положительных элементов L; L2 Ц из остальных элементов L.
+//2) –азработать функцию, котора€ удал€ет из списка L2 все отрицательные элементы.
+//3) –азработать функцию, котора€ в списке L1 узел с максимальным значением размещает перед первым узлом.
+
+
+struct Node // узел списка
+{
+	int element; // данные узла
+	Node* next; // указатель на следующий узел
+	Node(int _element) : element(_element), next(nullptr){};
+};
+
+struct List // однонаправленный список
+{
+	Node* first; // указатель на первый узел списка
+	Node* last; // указатель на последний узел списка
+	List() : first(nullptr), last(nullptr) {};
+	void addNode(int element) // добавление узла в список
+	{
+		Node* p = new Node(element); 
+		if (first == nullptr)
+		{
+			first = p;
+			last = p;
+		}
+		else
+		{
+			Node* temp = first;
+			first = p;
+			first->next = temp;
+		}
+	}
+	void delNode(int element) // удаление узла по элементу
+	{
+		if (first->element == element)
+		{
+			Node* temp = first;
+			first = first->next;
+			delete temp;
+			return;
+		}
+		else if (last->element == element)
+		{
+			Node* p = first;
+			while(p->next != last)
+			{
+				p = p->next;
+			}
+			delete last;
+			p->next = nullptr;
+			last = p;
+		}
+		else
+		{
+			Node* p = first;
+			Node* nxt = p->next;
+			while (nxt->element != element)
+			{
+				p = p->next;
+				nxt = p->next;
+			}
+			p->next = nxt->next;
+			delete nxt;
+		}
+	}
+	void moveToStart(int element)
+	{
+		Node* p = first;
+		Node* search = first;
+		while (search != nullptr)
+		{
+			if (search->element == element) break;
+			search = search->next;
+		}
+		if (search == first) return;
+		else if (search == last)
+		{
+			while (p->next != nullptr)
+			{
+				if (p->next == last)
+				{
+					last = p;
+					p->next = nullptr;
+					search->next = first;
+					first = search;
+					break;
+				}
+				p = p->next;
+			}
+		}
+		else
+		{
+			while (p->next != nullptr)
+			{
+				if (p->next == search)
+				{
+					p->next = search->next;
+					search->next = first;
+					first = search;
+					break;
+				}
+				p = p->next;
+			}
+		}
+	}
+	void outputList() // вывод списка
+	{
+		Node* p = first;
+		while (p != nullptr)
+		{
+			cout << p->element << " ";
+			p = p->next;
+		}
+	}
+};
+
+void createLists(List L, List& L1, List& L2) // заполнение списков L1 и L2
+{
+	Node* p = L.first;
+	while (p != nullptr)
+	{
+		if (p->element > 0) L1.addNode(p->element);
+		else L2.addNode(p->element);
+		p = p->next;
+	}
+}
+
+void delNegL2(List& L2) // удаление всех отрицательных элементов из списка L2
+{
+	Node* p = L2.first;
+	int k = 0;
+	while (p != nullptr)
+	{
+		k += 1;
+		p = p->next;
+	}
+	p = L2.first;
+	for (int i = 0; i < k; i++) 
+	{
+		int el = p->element;
+		if (el < 0)
+		{
+			p = p->next;
+			L2.delNode(el);
+		}
+		else p = p->next;
+	}
+}
+
+void maxElL1(List& L1) // перемещение узла с максимальным значением в начало списка
+{
+	Node* p = L1.first;
+	int max_el = 0;
+	while (p != nullptr)
+	{
+		if (p->element > max_el)
+		{
+			max_el = p->element;
+		}
+		p = p->next;
+	}
+	if (max_el == 0) return;
+	L1.moveToStart(max_el);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	List L, L1, L2;
+	cout << "¬ведите количество элементов в списке: ";
+	int n;
+	cin >> n;
+	cout << "¬веденные вами элементы: ";
+	if (n == 0) cout << endl;
+	else 
+	{
+		for (int i = 0; i < n; i++)
+		{
+			int el;
+			cin >> el;
+			L.addNode(el);
+		}
+	}
+	cout << "Ёлементы списка L: ";
+	L.outputList();
+	cout << endl;
+	createLists(L, L1, L2);
+	cout << "Ёлементы списка L1: ";
+	L1.outputList();
+	cout << endl;
+	cout << "Ёлементы списка L2: ";
+	L2.outputList();
+	cout << endl;
+	delNegL2(L2);
+	cout << "—писок L2 после удалени€ отрицательных элементов: ";
+	L2.outputList();
+	cout << endl;
+	maxElL1(L1);
+	cout << "—писок L1 после перемещени€ узла с максимальным значением в начало: ";
+	L1.outputList();
+	return(0);
+}
